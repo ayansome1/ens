8e445d0c29bbfda7e7f639c104dec11292dd9401
Sumit Guha <sumit03guha@gmail.com>
2023-03-30 02:24:30 +0530

    added APIs in frontend

---
+++ frontend/src/api/blockchain/getDomains-api.ts
@@ -0,0 +1,23 @@
+import { Alchemy, Network, OwnedNftsResponse } from 'alchemy-sdk';
+
+const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string;
+const ensAddress = process.env.NEXT_PUBLIC_ENS_ADDRESS_MAINNET as string;
+
+const config = {
+  apiKey: API_KEY,
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
+
+export const getDomainsApi = async (
+  owner: string
+): Promise<OwnedNftsResponse> => {
+  // Get all NFTs
+  const nfts = await alchemy.nft.getNftsForOwner(owner, {
+    contractAddresses: [ensAddress],
+  });
+  // Print NFTs
+  console.log(nfts);
+
+  return nfts;
+};
