4a897c33381c91839473120f0093cf87f60df6b7
Sumit Guha <sumit03guha@gmail.com>
2023-03-30 02:23:00 +0530

    next build files

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a07b5c224f482cac"; }
+/******/ 		__webpack_require__.h = function() { return "a49eab7f5e8eb4f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
